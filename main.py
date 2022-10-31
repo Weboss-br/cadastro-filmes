df65c0e4b4379c226fc0f3f06f2837137b0fe211
jeancnn <jean.niehues@proway.treina>
2022-10-31 19:14:35 -0300

    asdasd

---
+++ main.py
@@ -0,0 +1,10 @@
+from controller import buscaTitulo, cadastraTitulo, pegaDadosCadastro
+
+
+#print(buscaTitulo(input("digite o nome da obra")))
+
+
+titulo = pegaDadosCadastro()
+
+cadastraTitulo(titulo)
+#asdsdas
